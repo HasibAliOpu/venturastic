8e1551ec0c75723880dbf6fbcd35ead65ab34695
H_A _Opu <wwwhasib4@gmail.com>
2022-03-29 18:55:45 +0600

    navbar added

---
+++ src/components/CustomLink/CustomLink.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+function CustomLink({ children, to, ...props }) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link
+        style={{
+          fontWeight: "600",
+          marginRight: "36px",
+          color: match ? "#009CC4" : "black",
+          textDecoration: match ? "none" : "none",
+        }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+    </div>
+  );
+}
+
+export default CustomLink;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../images/icon/company-logo.png";
+import CustomLink from "../CustomLink/CustomLink";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="nav d-flex align-items-center justify-content-between ">
+      <img src={logo} alt="" />
+      <div className="nav-btn">
+        <CustomLink to="/">HOME</CustomLink>
+        <CustomLink to="/">WHAT WE WOULD DO</CustomLink>
+        <CustomLink to="/">CORPORATE GOVERNANCE</CustomLink>
+        <CustomLink to="/">FIRST PROJECT</CustomLink>
+        <CustomLink to="/">ABOUT US</CustomLink>
+        <CustomLink to="/">OUR TEAM</CustomLink>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
